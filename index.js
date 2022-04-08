03135aa807f0e92f1046ae3f937d760e379ae8bc
foghill <ben@thefoghill.com>
2022-04-07 20:32:26 -0400

    api key and endpoint as object literal

--- index.js
+++ index.js
@@ -18,4 +18,7 @@ function getLatLonFromZipCode(zipcode) {
     .catch((error) => console.error(error));
 }
 
-function getAirQualityFromLatLon(lat, lon = -74.0054) {}
+function getAirQualityFromLatLon(lat, lon = -74.0054) {
+  const API_KEY = "f530d82e051f70b8678adc31245d778d";
+  const ENDPOINT = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+}
