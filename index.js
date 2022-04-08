5ae07f084889db5a2b0b0cdccf5f2d7eb0153cf8
foghill <ben@thefoghill.com>
2022-04-07 20:34:08 -0400

    fetch inserted

--- index.js
+++ index.js
@@ -21,4 +21,8 @@ function getLatLonFromZipCode(zipcode) {
 function getAirQualityFromLatLon(lat, lon = -74.0054) {
   const API_KEY = "f530d82e051f70b8678adc31245d778d";
   const ENDPOINT = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  // fetch with endpoint
+  return fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {});
 }
