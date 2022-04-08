5e2bc20e25175272eb00ad470e1492c242bbe52f
foghill <ben@thefoghill.com>
2022-04-07 20:16:17 -0400

    get latlon

---
+++ index.js
@@ -0,0 +1,19 @@
+function getLatLonFromZipCode(zipcode) {
+  const API_KEY = "f530d82e051f70b8678adc31245d778d";
+  const ENDPOINT = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=${API_KEY}`;
+
+  return fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let airQuality = {};
+      console.log(`lat:${data.lat}, lon:${data.lon}`);
+      try {
+        let airQuality = getAirQualityFromLatLon(data.lat, data.lon);
+        // console.log ("getLatLonFromZipCode", JSON.stringify(airQuality));
+        return airQuality;
+      } catch (error) {
+        console.error(error);
+      }
+    })
+    .catch((error) => console.error(error));
+}
